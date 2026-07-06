cef5b2f01a35fd16dc3c52ef8cc3fd0aefacb48d
shivampcosmo <shivamp@sas.upenn.edu>
2026-07-06 12:22:16 -0400

    update

--- notebooks/CMASSfirstbin/new/check.py
+++
@@ -1,6 +0,0 @@
-import pickle as pk
-
-with open('/work/hdd/bdne/aacharya2/GODMAX/results/backlight_pkdgrav/CMASSfirstbin/new/sample_0/allmaps_sim_B12_nside512.pkl', 'rb') as f:
-    data = pk.load(f)
-
-print(data.keys())
