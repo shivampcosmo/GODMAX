f22546090f791651b3b3ed8f907c6c58561b0085
Anshuman Acharya <aacharya2@gh-login02.delta.ncsa.illinois.edu>
2026-08-04 08:13:54 -0500

    update

---
+++ notebooks/CMASSfirstbin/new/n1024/sbi_Cls/oneparam/lhs_pointsmaker.py
@@ -0,0 +1,24 @@
+import numpy as np
+from scipy.stats import qmc
+import pandas as pd
+
+# Define parameter ranges [min, max]
+space = {
+    'theta_ej_0': [1.0, 6.0],
+#    'nu_theta_ej_M': [-0.3, 0.0],
+#    'nu_theta_ej_z': [-3.0, 3.0],
+#    'mu_beta': [0.01, 1.5]
+}
+
+n_samples = 100
+sampler = qmc.LatinHypercube(d=len(space))
+sample = sampler.random(n=n_samples)
+
+# Scale to actual ranges
+l_bounds = [v[0] for v in space.values()]
+u_bounds = [v[1] for v in space.values()]
+sample_scaled = qmc.scale(sample, l_bounds, u_bounds)
+
+df = pd.DataFrame(sample_scaled, columns=space.keys())
+df.to_csv('lhs_samples.csv', index_label='sample_id')
+print("Generated "+str(n_samples)+" samples in lhs_samples.csv")
